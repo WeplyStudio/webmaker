299f1ed92d787cc154a1ea948fb5c0fbc46e6a97
WebKarya <146603597+WebKarya@users.noreply.github.com>
2023-12-17 08:13:15 +0700

    update api.js

--- api.js
+++ api.js
@@ -1,13 +1,1 @@
-// script.js
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Code ini akan dieksekusi setelah halaman selesai dimuat
-
-    // Menampilkan pesan pada console browser
-    console.log('Selamat datang di CiptaHost API!');
-
-    // Menampilkan pesan di halaman HTML
-    var messageElement = document.createElement('p');
-    messageElement.textContent = 'Selamat datang di CiptaHost API!';
-    document.body.appendChild(messageElement);
-});
+console.log('Selamat datang di CiptaHost API!');
