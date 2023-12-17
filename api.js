f9a8e562b92b9f94de7d5d61a1b0984a8206a7c0
WebKarya <146603597+WebKarya@users.noreply.github.com>
2023-12-17 08:06:22 +0700

    update api.js

---
+++ api.js
@@ -0,0 +1,2 @@
+Selamat datang di CiptaHost API!. Gunakan API kami secara bijak!
+{"clientid":"masukkan client id anda", "token":"masukkan token anda", "type":"masukkan jenis api yang ingin anda gunakan"}
